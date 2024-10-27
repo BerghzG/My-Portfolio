826733f96a78c042c046d00a517244b7fae61f0f
Berghz G <berghzg@gmail.com>
2024-10-27 16:42:36 -0300

    basico do servidor e do ejs

---
+++ AnimeFinder/app.js
@@ -0,0 +1,25 @@
+import express from "express"
+import bodyParser from "body-parser"
+import axios from "axios"
+import formData from "form-data"
+import multer from "multer"
+import { dirname } from "path"
+import path from "path"
+import { fileURLToPath } from "url"
+
+
+const upload = multer({ dest: './uploads/' })
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const app = express()
+const port = 3000
+
+app.use(bodyParser.json)
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static("public"))
+
+app.set("views", path.join(__dirname, "views"))
+app.set("views engine", "ejs")
+
+app.get("/", (req, res) => {
+    res.render("index.ejs")
+})
