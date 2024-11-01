100044f917cbbde274fd201d10d74a1f8ede2ae7
Berghz G <berghzg@gmail.com>
2024-10-31 22:32:40 -0300

    novas coisas

--- AnimeFinder/app.js
+++ AnimeFinder/app.js
@@ -1,25 +1,28 @@
-import express from "express"
-import bodyParser from "body-parser"
-import axios from "axios"
-import formData from "form-data"
-import multer from "multer"
-import { dirname } from "path"
-import path from "path"
-import { fileURLToPath } from "url"
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+import formData from "form-data";
+import multer from "multer";
+import { dirname } from "path";
+import path from "path";
+import { fileURLToPath } from "url";
 
+const upload = multer({ dest: './uploads/' });
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = 3000;
 
-const upload = multer({ dest: './uploads/' })
-const __dirname = dirname(fileURLToPath(import.meta.url))
-const app = express()
-const port = 3000
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-app.use(bodyParser.json)
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static("public"))
-
-app.set("views", path.join(__dirname, "views"))
-app.set("views engine", "ejs")
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.render("index.ejs")
-})
+    res.render("index.ejs");
+});
+
+app.listen(port, () => {
+    console.log(`Sá merda tá funcionando na porta ${port}`);
+});
