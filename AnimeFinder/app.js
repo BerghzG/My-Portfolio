a1d8e8a416e26adbdb39d7aba35e9eec7a6b49e9
Berghz G <berghzg@gmail.com>
2024-11-15 04:20:26 -0300

    Adicionado coisas ao server do animeFinder

--- AnimeFinder/app.js
+++ AnimeFinder/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
 import formData from "form-data";
+import fs from "fs";
 import multer from "multer";
 import { dirname } from "path";
 import path from "path";
@@ -23,9 +24,30 @@ app.get("/", (req, res) => {
     res.render("index.ejs");
 });
 
+// Trace Moe API
+// https://api.trace.moe/search
+
+app.post("/search", upload.single('image'), (req, res) => {
+    if (!req.file) {
+        return res.status(400).send('Por favor, envie uma imagem.');
+    }
+
+
+    const formData = new formData()
+    formData.append("image", req.file.buffer)
+
+    axios.post("https://api.trace.moe/search", formData, { headers: { 'Content-Type': 'multipart/form-data'}, })
+    .then((response) => {
+        const animeInfo = response.data
+        const animes = JSON.parse(fs.readFileSync('animes.json'))
+    })
+
+    animes.push({
+        
+    })
+})
+
+
 app.listen(port, () => {
     console.log(`Sá merda tá funcionando na porta ${port}`);
 });
-
-
-// Trace Moe API
